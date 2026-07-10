d312062bbf13aa8092160d860a167367aee66440
kfn8dkodemonkey <kaffeinatedkodemonkey@gmail.com>
2026-07-10 17:25:42 -0600

    update mongo connection

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require("dotenv").config({ path: ".env.development" }
 
 const path = require("path")
 const { MongoClient } = require("mongodb")
